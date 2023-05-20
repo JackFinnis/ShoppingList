1a28a94d41d3f6e2fbfa13de78cf84e27c5c556b
Jack Finnis <jack.finnis@icloud.com>
2023-05-20 23:08:59 +0100

    Initial Commit

---
+++ ShoppingList/ShoppingListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingListApp.swift
+//  ShoppingList
+//
+//  Created by Jack Finnis on 20/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
